91a427cabbd94d460aea66bfe135dd15e8ccb4c4
Sonu Vyavahare <sonuvyavahare2021@gmail.com>
2023-05-23 08:52:49 +0530

    Updating B2

--- src/BasicProg/StringDemo.java
+++ src/BasicProg/StringDemo.java
@@ -3,7 +3,8 @@ package BasicProg;
 public class StringDemo {
 	
 	public static void main(String[] args) {
-		System.out.println("I am in main method...........");
-		System.out.println("Conflict resolved");
+		System.out.println("I am in main method.........");
+		System.out.println("Creating conflict B2");
+		System.out.println("Hello B2");
 	}
 }	

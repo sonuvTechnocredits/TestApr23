e4c7ffbfb07bdadec80d8491f246f2f8d8b2c39c
Sonu Vyavahare <sonuvyavahare2021@gmail.com>
2023-05-23 08:11:27 +0530

    Branch A1 changes.

--- src/BasicProg/StringDemo.java
+++ src/BasicProg/StringDemo.java
@@ -3,6 +3,8 @@ package BasicProg;
 public class StringDemo {
 	
 	public static void main(String[] args) {
-		System.out.println("I am in main method");
+		System.out.println("I am in main method of Branch A1");
+		System.out.println("Creating conflict scenario from A1");
+		System.out.println("Hello A1");
 	}
 }
